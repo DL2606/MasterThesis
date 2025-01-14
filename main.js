5831f2415e8489467b544e18b53772b57c627364
DL2606 <denis.ljutic@student.hs-rm.de>
2025-01-14 11:34:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ const ajaxCall = (apiKey, prompt) => {
       data: JSON.stringify({
         model: "gpt-4o-mini",
         messages: [{ role: "user", content: dynamicPrompt }],
-        max_tokens: 10000,
+        max_tokens: 1000,
         n: 1,
-        top_p: 0.1,
+        temperature: 0.1,
       }),
       headers: {
         "Content-Type": "application/json",
